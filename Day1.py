d33ac6f393c5a1813c6827727f8d3dcc6c9c1961
John Foster <focyte@gmail.com>
2022-12-08 22:38:48 +0000

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy as sci
+import pandas as pd
+import sys
+
+elf = pd.read_csv('Day1.txt', skip_blank_lines=False, header=None, names=["Cal"])
+
+pd.set_option('display.max_rows', elf.shape[0]+1)
+
+g = elf.Cal.isna()
+Calories=elf[~g].groupby(g.cumsum()).Cal.apply(list).tolist()
+print(Calories)
+print(type(Calories))
+
+totals=[sum(i) for i in Calories]
+
+print(max(totals))
